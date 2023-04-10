2a474f816bba76550fd492930f0880ddae26e455
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-04-10 15:46:54 -0500

    Updated JavaScript and link

---
+++ Develop/script.js
@@ -0,0 +1,57 @@
+var startBtn = document.querySelector("#start");
+
+const Questions = [
+    //Question 1
+    Question1, "Commonly used data types DO Not include:",
+    
+    Option1, "Strings",
+    Option2, "Booleans",
+    Option3, "Alerts",
+    Option4, "Numbers",
+
+    Answer, "Alerts",
+
+    //Question 2
+    Question2, "The condition in an if/else statement is enclosed with______?",
+    
+    Option1, "Quotes",
+    Option2, "Curly Brackets",
+    Option3, "Parenthesis",
+    Option4, "Square Brackets",
+
+    Answer, "Boolean",
+
+    //Question 3
+    Question3, "Arrays in JavaScript can be used to store______?",
+
+    Option1, "Numbers and Strings",
+    Option2, "Other Arrays",
+    Option3, "Booleans",
+    Option4, "All of the above",
+
+    Answer, "All of the above",
+
+    //Question 4
+    Question4, "String values must be enclosed within ______ when being assigned to variables?",
+
+    Option1, "Commas",
+    Option2, "Curly Brackets",
+    Option3, "Quotes",
+    Option4, "Parenthesis",
+
+    Answer, "Quotes",
+
+    //Question 5
+    Question5, "A very useful tool used during development and debugging for printint content to the debugges is?",
+
+    Option1, "JavaScript",
+    Option2, "Terminal/Bash",
+    Option3, "for loops",
+    Option4, "console.log",
+
+    Answer, "console.log",
+];
+
+startBtn.onclick = function(Questions){
+
+}
