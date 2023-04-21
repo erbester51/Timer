6f5d056547fd317b785e17de656fead26610c948
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-04-21 12:19:32 -0500

    Updated JavaScript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,55 @@
-var startBtn = document.querySelector("#start");
+var startBtn = document.getElementById("#start");
 
-const Questions = [
+
+const questions = [
     //Question 1
-    Question1, "Commonly used data types DO Not include:",
+   {
+    question1: "Commonly used data types DO Not include:",
     
-    Option1, "Strings",
-    Option2, "Booleans",
-    Option3, "Alerts",
-    Option4, "Numbers",
-
-    Answer, "Alerts",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+  
+    answer: "Alerts"
+   },
 
     //Question 2
-    Question2, "The condition in an if/else statement is enclosed with______?",
+    {
+    question2: "The condition in an if/else statement is enclosed with______?",
     
-    Option1, "Quotes",
-    Option2, "Curly Brackets",
-    Option3, "Parenthesis",
-    Option4, "Square Brackets",
+    choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
 
-    Answer, "Boolean",
+    answer: "Boolean"
+    },
 
     //Question 3
-    Question3, "Arrays in JavaScript can be used to store______?",
+    {
+    question3: "Arrays in JavaScript can be used to store______?",
 
-    Option1, "Numbers and Strings",
-    Option2, "Other Arrays",
-    Option3, "Booleans",
-    Option4, "All of the above",
+    choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
 
-    Answer, "All of the above",
+    answer: "All of the above"
+    },
 
     //Question 4
-    Question4, "String values must be enclosed within ______ when being assigned to variables?",
+    {
+    question4: "String values must be enclosed within ______ when being assigned to variables?",
 
-    Option1, "Commas",
-    Option2, "Curly Brackets",
-    Option3, "Quotes",
-    Option4, "Parenthesis",
-
-    Answer, "Quotes",
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    
+    answer: "Quotes"
+    },
 
     //Question 5
-    Question5, "A very useful tool used during development and debugging for printint content to the debugges is?",
+    {
+    question5: "A very useful tool used during development and debugging for printint content to the debugges is?",
 
-    Option1, "JavaScript",
-    Option2, "Terminal/Bash",
-    Option3, "for loops",
-    Option4, "console.log",
+    choices: ["JavaScript", "Terminal/Bash", "for loops", "console.log"],
 
-    Answer, "console.log",
+    answer: "console.log"
+    }
 ];
+
+//Keep track of the user's score
+let score = 0;
 
 startBtn.onclick = function(Questions){
 
